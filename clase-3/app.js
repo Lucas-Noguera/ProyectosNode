8443962cb94e8960249eba6e5f7da834cf65ded5
Lucas-Noguera <lucas2005n@gmail.com>
2025-02-27 14:59:37 -0300

    Clase 3

--- clase-3/app.js
+++ clase-3/app.js
@@ -4,12 +4,23 @@ const movies = require('./movies.json')
 const app = express()
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' })
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
 })
 
-app.get('/movies', (req, res) => {
-  res.json(movies)
+app.get('/movies/:id', (req, res) => {
+  console.log(req)
+  const id = req.params
+  console.log(id)
+  const movie = movies.find((movie) => movie.id === id.id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
 })
 
 const PORT = process.env.PORT ?? 1234
