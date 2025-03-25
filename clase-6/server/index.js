1959d09e1da4dbcb06f8fde95fdded075bd2bc85
Lucas-Noguera <lucas2005n@gmail.com>
2025-03-25 15:00:47 -0300

    Actualizacion

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -1,14 +1,28 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
 const port = process.env.PORT || 3000
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+  console.log('A user connected')
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected')
+  })
+})
+
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
-  res.sendFile(process.cwd())
+  res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running port http://localhost:${port}`)
 })
