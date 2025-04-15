26a4b8ed6af7ae19f817e63b9ff7f9044a892d64
Lucas-Noguera <lucas2005n@gmail.com>
2025-04-15 11:27:32 -0300

    clase terminada

---
+++ node-js-user-auth/config.js
@@ -0,0 +1,4 @@
+export const config = {
+  port: process.env.PORT || 3000,
+  jwtSecret: 'this-is-an-awesome-secret-key'
+}
--- node-js-user-auth/index.js
+++ node-js-user-auth/index.js
@@ -1,33 +1,60 @@
 import express from 'express'
+import jws from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 import { UserRepository } from './user-repository.js'
 import { validateUser } from './schemas/users.js'
+import { config } from './config.js'
 
 const app = express()
 
-const port = process.env.PORT || 3000
-
 app.set('view engine', 'ejs')
 app.use(express.json())
+app.use(cookieParser())
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+  req.session = { user: null }
 
-app.get('/', (req, res) => {
-  res.render('index')
+  try {
+    const data = jws.verify(token, config.jwtSecret)
+    req.session.user = data
+  } catch {}
+
+  next()
 })
 
-app.post('/login', (req, res) => {
-  const result = validateUser(req.body)
+app.get('/', (req, res) => {
+  const { user } = req.session
+  res.render('index', user)
+})
 
-  if (result.error) {
-    return res.status(400).json({ error: result.error.issues })
+app.post('/login', async (req, res) => {
+  try {
+    const result = validateUser(req.body)
+
+    if (result.error) {
+      return res.status(400).json({ error: result.error.issues })
+    }
+
+    const { username, password } = result.data
+    const user = await UserRepository.login({ username, password })
+
+    const token = jws.sign(
+      { id: user._id, username: user.username },
+      config.jwtSecret,
+      { expiresIn: '1h' }
+    )
+
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 3600 * 1000 // 1 hour
+      })
+      .send({ user, token })
+  } catch (error) {
+    res.status(400).send(error.message)
   }
-
-  const { username, password } = result.data
-  UserRepository.login({ username, password })
-    .then((user) => {
-      res.send({ user })
-    })
-    .catch((error) => {
-      res.status(400).send(error.message)
-    })
 })
 
 app.post('/register', async (req, res) => {
@@ -46,11 +73,19 @@ app.post('/register', async (req, res) => {
     res.status(400).send(error.message)
   }
 })
-app.post('/logout', (req, res) => {})
-app.get('/protected', (req, res) => {
-  res.render('protected')
+
+app.post('/logout', (req, res) => {
+  res
+    .clearCookie('access_token')
+    .send({ message: 'Session destroyed' })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/protected', (req, res) => {
+  const { user } = req.session
+  if (!user) return res.status(403).send('Access not authorized')
+  res.render('protected', user)
+})
+
+app.listen(config.port, () => {
+  console.log(`Example app listening at http://localhost:${config.port}`)
 })
